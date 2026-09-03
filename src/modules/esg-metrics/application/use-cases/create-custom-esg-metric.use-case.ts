5981e2a5ba1cf6d4ce3ff2129aea0b0baa764bdb
AugustoMissiaggia <augustomissiaggiap@gmail.com>
2026-09-03 20:49:26 -0300

    fix: inject esg metric use case dependencies

--- src/modules/esg-metrics/application/use-cases/create-custom-esg-metric.use-case.ts
+++ src/modules/esg-metrics/application/use-cases/create-custom-esg-metric.use-case.ts
@@ -1,9 +1,11 @@
+import { Injectable } from '@nestjs/common';
 import type { EsgMetricEntity } from '../../domain/entities/esg-metric.entity';
 import {
   EsgMetricRepository,
   type EsgMetricData,
 } from '../../domain/repositories/esg-metric.repository';
 
+@Injectable()
 export class CreateCustomEsgMetricUseCase {
   constructor(private readonly esgMetricRepository: EsgMetricRepository) {}
 
